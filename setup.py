8e199515febe87ac06e81c58d9db48590749959d
Miłosz Gajewski <mgmilo100@gmail.com>
2023-04-25 20:48:03 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'tswr_awsim'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='AMMM',
+    maintainer_email='ammm@pp.com',
+    description='TSwR Project - AWSIM',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test_script = tswr_awsim.test_script:main',
+        ],
+    },
+)
